8de8130dea320129b8fe1f6a8805554db5d6ac9e
YanaMasalova <anulamasalova@gmail.com>
2026-01-28 22:43:22 +0600

    add functions for better structures

--- main.py
+++ main.py
@@ -4,62 +4,80 @@ from url_request import name_of_data, valutes_of_data
 from read_statistics import old_valutes_of_data, info_is_ready
 
 
-info_list = []
-if info_is_ready:
-    for i in range(len(valutes_of_data)):
-        if old_valutes_of_data[i] != valutes_of_data[i]:
-            price_delta = old_valutes_of_data[i] - valutes_of_data[i]
-            if price_delta < 0:
-                info = "дороже"
+def save_change_prices():
+    """Внесение информации об изменениях цен на валюты в txt файл."""
+    info_list = []
+    if info_is_ready:
+        for i in range(len(valutes_of_data)):
+            if old_valutes_of_data[i] != valutes_of_data[i]:
+                price_delta = old_valutes_of_data[i] - valutes_of_data[i]
+                if price_delta < 0:
+                    info = "дороже"
+                else:
+                    info = "дешевле"
+                info_list.append(
+                    f"\"{name_of_data[i]}\" изменил цену c "
+                    f"{old_valutes_of_data[i]} руб. на {valutes_of_data[i]} "
+                    f"руб. Стало {info} на: {abs(price_delta):.3f} руб."
+                )
             else:
-                info = "дешевле"
+                info_list.append(
+                    f"Изменений цены для \"{name_of_data[i]}\" нет."
+                    )
+    else:
+        for i in range(len(valutes_of_data)):
             info_list.append(
-                f"\"{name_of_data[i]}\" изменил цену c "
-                f"{old_valutes_of_data[i]} руб. на {valutes_of_data[i]} руб. "
-                f"Стало {info} на: {abs(price_delta):.3f} руб."
+                f"Недостаточно информации для анализа \"{name_of_data[i]}\"."
+                )
+    return info_list
+
+
+def save_info_prices(info_list):
+    """Внесение информации о курсах валют в txt файл."""
+    now = datetime.strftime(datetime.now(), "%H:%M:%S %d.%m.%Y")
+
+    with open("statistics.txt", "a", encoding="utf-8") as file:
+        file.write(f"============= Дата: {now}. =============\n")
+        for i in range(len(valutes_of_data)):
+            file.write(f"{valutes_of_data[i]} руб. - {name_of_data[i]}.\n")
+        file.write("============== Информация об изменениях ==============\n")
+        for info in info_list:
+            file.write(
+                f"{info}\n"
             )
-        else:
-            info_list.append(f"Изменений цены для \"{name_of_data[i]}\" нет.")
-else:
-    for i in range(len(valutes_of_data)):
-        info_list.append(
-            f"Недостаточно информации для анализа \"{name_of_data[i]}\"."
-            )
-    print("Для анализа курсов валют информации недостаточно...")
-
-
-now = datetime.strftime(datetime.now(), "%H:%M:%S %d.%m.%Y")
-
-with open("statistics.txt", "a", encoding="utf-8") as file:
-    file.write(f"============= Дата: {now}. =============\n")
-    for i in range(len(valutes_of_data)):
-        file.write(f"{valutes_of_data[i]} руб. - {name_of_data[i]}.\n")
-    file.write("============== Информация об изменениях ==============\n")
-    for info in info_list:
         file.write(
-            f"{info}\n"
-        )
-    file.write(
-            "============ Информация от Центробанка РФ ============\n\n\n"
-        )
+                "============ Информация от Центробанка РФ ============\n\n\n"
+            )
 
 
-window = Tk()
-window.title("RublesToDollars")
-# window.geometry("500x500")
+def visual():
+    """Создание программы с курсами валют для визуала."""
+    window = Tk()
+    window.title("RublesToDollars")
+    # window.geometry("500x500")
 
-icon = PhotoImage(file="img/yana_logo_1.png")
-window.iconphoto(True, icon)
+    icon = PhotoImage(file="img/yana_logo_1.png")
+    window.iconphoto(True, icon)
 
-frm = ttk.Frame(window, padding=40)
-frm.grid()
+    frm = ttk.Frame(window, padding=40)
+    frm.grid()
 
-# Вывод текста с курсами валют в центре окна приложения.
-for i in range(len(valutes_of_data)):
+    # Вывод текста с курсами валют в центре окна приложения.
+    for i in range(len(valutes_of_data)):
+        ttk.Label(
+            frm,
+            text=f"{name_of_data[i]}: {valutes_of_data[i]} руб."
+        ).grid(column=0, row=i)
+
     ttk.Label(
         frm,
-        text=f"{name_of_data[i]}: {valutes_of_data[i]} руб."
-    ).grid(column=0, row=i)
+        text="Информация с сайта Центробанка РФ."
+        ).grid(column=0, row=6)
 
-ttk.Label(frm, text="Информация с сайта Центробанка РФ.").grid(column=0, row=6)
-window.mainloop()
+    window.mainloop()
+
+
+info_list = save_change_prices()
+save_change_prices()
+save_info_prices(info_list)
+visual()
--- read_statistics.py
+++ read_statistics.py
@@ -1,30 +1,36 @@
 from string import digits
 
 
-info_is_ready = False
-with open("statistics.txt", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-    count_lines = len(lines)
-
-if count_lines > 1:
-    info_is_ready = True
-    start_file = count_lines - 14
-    end_file = count_lines - 9
-
-    lines_list = []
-    for stroka in lines[start_file:end_file]:
-        line = stroka.split()
-        for el in line:
-            if el[0] in digits and ":" not in el and el.count(".") <= 1:
-                lines_list.append(line)
-
-    old_valutes_of_data = [
-        float(lines_list[i][0]) for i in range(len(lines_list))
-        ]
-else:
+def get_old_info_prices():
+    """Получение информации из txt файла со старым курсом валют."""
     info_is_ready = False
-    old_valutes_of_data = []
+    with open("statistics.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+        count_lines = len(lines)
+
+    if count_lines > 1:
+        info_is_ready = True
+        start_file = count_lines - 14
+        end_file = count_lines - 9
+
+        lines_list = []
+        for stroka in lines[start_file:end_file]:
+            line = stroka.split()
+            for el in line:
+                if el[0] in digits and ":" not in el and el.count(".") <= 1:
+                    lines_list.append(line)
+
+        old_valutes_of_data = [
+            float(lines_list[i][0]) for i in range(len(lines_list))
+            ]
+    else:
+        info_is_ready = False
+        old_valutes_of_data = []
+    return old_valutes_of_data, info_is_ready
 
 
-if __name__ == "__main__":
-    print(lines_list)
+old_valutes_of_data, info_is_ready = get_old_info_prices()
+
+
+# if __name__ == "__main__":
+#     print(lines_list)
